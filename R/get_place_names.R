863f7e56bf633b58ef6786ec6785971630af87cc
pranavanba <pranavanba@gmail.com>
2023-07-03 12:24:07 -0700

    Specify library names for used functions

--- R/get_place_names.R
+++ R/get_place_names.R
@@ -1,12 +1,9 @@
-library(rvest)
-library(xml2)
-
 get_place_names <- function(guide_url_file_path) {
-  guide_page <- read_html(readLines(guide_url_file_path))
+  guide_page <- xml2::read_html(readLines(guide_url_file_path))
 
   place_names <- guide_page %>%
-    html_elements('.mw-collection-place-name.mw-dir-label') %>%
-    html_text()
+    rvest::html_elements('.mw-collection-place-name.mw-dir-label') %>%
+    rvest::html_text()
 
   return(place_names)
 }
